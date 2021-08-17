48d71943eb358c8ea1d27258d1dd1e4c022080b6
Gisleudo <gisleudo.cmf@gmail.com>
2021-08-17 19:53:55 -0300

    Segundo Comit implrmentação de modulos  17/08/2021

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-import investpy
-import pandas as pd
+from module import Ticker
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
 
-stock = 'FLRY3'
-country = 'brazil'
-#x = investpy.stocks.get_stock_company_profile(stock, country=country, language='english')
-#y = investpy.stocks.get_stock_dividends(stock, country)
-z = investpy.stocks.get_stock_information(stock, country, as_json=True)
-c = [q  for q in z.items()]
+data_inicial = '16/08/2021'
+data_final = '17/08/2021'
+pais = 'brazil'
+stock = Ticker('FBOK34', pais)
 
-#{key:value for (key,value) in dictonary.items()}
 
-w = {key:value for (key,value) in z.items() }
+hist = stock.dados_historicos(data_inicial,data_final, interval = 'Daily')
 
-print(z.get('Beta'))
+print(hist)
--- module.py
+++ module.py
@@ -1,15 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import investpy
+
+
 class Ticker:
 
-	def __init__(self, ticker):
+	def __init__(self, ticker, pais):
 		self.ticker = ticker
+		self.pais = pais
 	
-
-	def calcula_beta(self):
+	# Retornar o Beta Atualizado de um espaço de tempo definido, padrão 5 meses
+	def beta(self):
 		pass
 	
 
 	def bollinger_band(self):
 		pass
 
-	def dados_historicos(self, data_inicial, data_final):
-		pass
+	# Criado como redundancia para funcionar como metodo do objeto Ticker
+	#dd/mm/yyyy # interval = 'Daily, Weekly or Monthly'
+	def dados_historicos(self, data_inicial, data_final, interval = 'Monthly'):
+		return investpy.stocks.get_stock_historical_data(self.ticker, self.pais ,data_inicial, data_final, as_json = False, order = 'descending', interval=interval)
+
+
+
+
+
+# ---- TODO ----  
+# Confirmar se terá de funcionar sempre online( fazendo pedidos de api pelo investpy ou alphavantage )
+# Offline ( fazendo copias diarias dos preços para manter uma data base de testes)
